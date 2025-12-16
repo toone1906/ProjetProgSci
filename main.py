6dacaacd7b8532693e96c56ee192951f3ad6acf3
Theau LEFRANC <lefrancthea@gmail.com>
2025-12-16 12:14:27 +0100

    chargement pmm_itrf

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ earth = pd.read_fwf("data/Earth_Parameters.dat", skiprows = 8)
 
 GSRM = pd.read_fwf("data/GSRM_strain.txt", skiprows=24,  columns=[] )
 ITRF_2020 = pd.read_fwf("data/ITRF2020_GNSS.SSC.txt", skiprows=8,  names=["DOMES NB", "SITE NAME", "TECH","ID", "X/Vx","Y/Vy","Z/Vz","Sigma_x","Sigma_y","Sigma_z","SOLN","DATA_START","DATA_END"] )
-pmm_itrf =  pd.read_fwf("data/pmm_itrf.txt", skiprows=4,  names=["Plate", "Name", "NS","Omega_x", "Omega_y","Omega_z","Omega","WRMS","Sigma_y","s_Omega_x","s_Omega_y","s_Omega_z","s_Omega"] )
+pmm_itrf = pd.read_csv("data/pmm_itrf.txt",sep='\s+',skiprows=4,names=["Plate", "Name", "NS","Omega_x", "Omega_y","Omega_z","Omega","WRMS","Sigma_y","s_Omega_x","s_Omega_y","s_Omega_z","s_Omega"])
 
-print(pmm_itrf)
-
-print()
+forte_deformation = GSRM[]
