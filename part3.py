cf98827a805ba504f7105f75641dd9d24e52a1ef
Melchior Ganet <ganet.melchior@gmail.com>
2026-01-09 11:43:33 +0100

    suite part3

--- part3.py
+++ part3.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def distance_haversine(phi1,phi2,lambda1,lambda2): 
     Rt = 6378E3
-    return 2*Rt*m.asin(m.sqrt(m.sin((phi2-phi1)/2)**2+ m.cos(phi1)*m.cos(phi2)*m.sin((lambda2 - lambda1)/2)**2)))
+    return 2*Rt*m.asin(m.sqrt(m.sin((phi2-phi1)/2)**2+ m.cos(phi1)*m.cos(phi2)*m.sin((lambda2 - lambda1)/2)**2))
 
 
 
@@ -12,8 +12,12 @@ GSRM = pd.read_fwf("data/GSRM_strain.txt", skiprows=25,  names=["lat","long", "e
 
 GSRM["deformation"] = np.sqrt(GSRM["exx"]**2 + GSRM["eyy"]**2 + 2 * GSRM["exy"]**2)
 
-forte_deformation = GSRM[GSRM["deformation"] > 50]
+GRSM = GSRM[GSRM["deformation"] > 50]
 
-forte_deformation = forte_deformation.sort_values(by=["lat", "long", "deformation"], ascending=True)
+GRSM = GRSM.sort_values(by=["lat", "long", "deformation"], ascending=True)
 
-# forte_deformation_sans_doublons = forte_deformation.drop_duplicates(subset=["lat","long"], keep="last")
+GRSM = GRSM.drop_duplicates(subset=["lat","long"], keep="last")
+
+print(GRSM)
+
+
