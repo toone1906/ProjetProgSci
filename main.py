4e9aa53de51ed1248c59dc195e2490550f32941e
Melchior Ganet <ganet.melchior@gmail.com>
2026-01-09 11:30:26 +0100

    debut part3 pour merge

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ import math
 
 import json
 import part2
-#earth = pd.read_fwf("data/Earth_Parameters.dat", skiprows = 8)
+
+
+with open("data/Earth_Parameters.dat", 'r') as f: 
+    txt = f.readlines()
+earth = {}
+for i in range (0,len(txt)-1,2):
+    earth[txt[i][2:-1]] = txt[i+1][:-1]
 
 
 with open("data/Tectonic_Plates.geojson") as f:
@@ -19,14 +25,7 @@ with open("data/Tectonic_Plates.geojson") as f:
 ITRF_2020 = pd.read_fwf("data/ITRF2020_GNSS.SSC.txt", skiprows=8,  names=["DOMES NB", "SITE NAME", "TECH","ID", "X/Vx","Y/Vy","Z/Vz","Sigma_x","Sigma_y","Sigma_z","SOLN","DATA_START","DATA_END"] )
 pmm_itrf = pd.read_csv("data/pmm_itrf.txt",sep='\s+',skiprows=4,names=["Plate", "Name", "NS","Omega_x", "Omega_y","Omega_z","Omega","WRMS","Sigma_y","s_Omega_x","s_Omega_y","s_Omega_z","s_Omega"])
 
-'''
-filtre_pos = (ITRF_2020['DATA_END'] == "00:000:00000") | ((ITRF_2020['DATA_END'].isna()) & (ITRF_2020['SITE NAME'].notna()) )
-#je prends une condition sur data_end et les lignes en dessous qui correspond à la vitesse doivent être retirer : notna() enlèbe tout les NaN
 
-
-last_version_position1 = ITRF_2020.loc[filtre_pos, ['SITE NAME', 'X/Vx','Y/Vy','Z/Vz'] ]
-last_version_position1 = last_version_position1.drop_duplicates(subset = 'SITE NAME', keep = 'first')
-'''
 
 last_version_position2 = ITRF_2020.loc[(ITRF_2020['SITE NAME'].notna())]
 #pour retirer toute les lignes qui traitent de la vitesse (la ville n'est pas renseigné donc NaN)
@@ -43,15 +42,16 @@ last_version_position2 = last_version_position2.drop_duplicates(subset= 'SITE NA
 #la vitesse à la meme signature d'antenne que la position. 
 last_version_vitesse = ITRF_2020.drop_duplicates(subset= 'DOMES NB', keep='last')
 last_version_vitesse = last_version_vitesse.loc[:, ['DOMES NB', 'X/Vx', 'Y/Vy','Z/Vz']]
-# print(last_version_vitesse)
+
+print(last_version_vitesse)
 
 
 #Q3 
 # Faire une fonction qui nous redonne  λ et φ lorsqu'on reçoit des coordonnées cartésienne en 3 dimension. 
 
 def xyz_to_pol(x,y,z): 
-    ae = 6378137.0
-    fe = 1/298.257223563
+    ae = float(earth['ae'])
+    fe = 1/float(earth['1/fe'])
     ee2 = fe*(2-fe)
     r = math.sqrt(x**2 + y**2 + z**2)
     mu = math.atan(z/math.sqrt(x**2+y**2) * ((1-fe) + ae*ee2/r))
@@ -62,14 +62,25 @@ def xyz_to_pol(x,y,z):
 #Q4 doit rajouter deux colonnes où on
 def rad_to_degres(rad): 
     return rad*180/math.pi
-last_version_position2['lon(rad)'] = last_version_position2.apply(lambda r: xyz_to_pol(r['X/Vx'],r['Y/Vy'],r['Z/Vz'])[0], axis = 1)
-last_version_position2['lat(rad)'] = last_version_position2.apply(lambda r: xyz_to_pol(r['X/Vx'],r['Y/Vy'],r['Z/Vz'])[1], axis = 1)
-
-last_version_position2['lon(degres)'] = last_version_position2.apply(lambda r : rad_to_degres(r['lon(rad)']),axis = 1 )
-last_version_position2['lat(degres)'] = last_version_position2.apply(lambda r : rad_to_degres(r['lat(rad)']), axis = 1)
 
 
-#print(last_version_position2)
+def radlon(r): 
+    return xyz_to_pol(r['X/Vx'],r['Y/Vy'],r['Z/Vz'])[0]
+def radlat(r): 
+    return xyz_to_pol(r['X/Vx'],r['Y/Vy'],r['Z/Vz'])[1]
+def degreslon(r): 
+    return rad_to_degres(xyz_to_pol(r['X/Vx'],r['Y/Vy'],r['Z/Vz'])[0])
+def degreslat(r): 
+    return rad_to_degres(xyz_to_pol(r['X/Vx'],r['Y/Vy'],r['Z/Vz'])[1])
+
+last_version_position2['lon(rad)'] = last_version_position2.apply(radlon, axis = 1)
+last_version_position2['lat(rad)'] = last_version_position2.apply(radlat, axis = 1)
+
+last_version_position2['lon(degres)'] = last_version_position2.apply(degreslon, axis = 1 )
+last_version_position2['lat(degres)'] = last_version_position2.apply(degreslat, axis = 1)
+
+
+print(last_version_position2)
 
 
 #1.2
@@ -127,7 +138,7 @@ dico_plaques_pmm_noms = {
 
 # 2.1
 
-last_version_position2['Plate'] = part2.isIn_geoPandas(last_version_position2, dico_plaques_pmm_noms)
+last_version_position2['Plate'] = part2.isIn_mat(last_version_position2, dico_plaques_pmm_noms)
 
 print("\nAffichage des stations avec leur plaque tectonique associée :")
 print(last_version_position2[['SITE NAME', 'Plate']])
