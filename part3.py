a35c0918c5424025c9c96fbda5d3da3d97b68ea3
Melchior Ganet <ganet.melchior@gmail.com>
2026-01-10 14:20:05 +0100

    part itrf

--- part3.py
+++ part3.py
@@ -17,7 +17,7 @@ def deg_rad(deg):
 
 
 
-def proxi(GRSM,ITDF):   
+def proxi(GRSM,ITRF):   
 
     GRSM['lat_rad'] = np.deg2rad(GRSM['lat'])
     GRSM['long_rad'] = np.deg2rad(GRSM['long'])
@@ -26,11 +26,11 @@ def proxi(GRSM,ITDF):
     phi_def = GRSM['lat_rad'].values
     lambda_def = GRSM['long_rad'].values
     
-    phi_stations = ITDF['lat(rad)'].values
-    lambda_stations = ITDF['lon(rad)'].values
+    phi_stations = ITRF['lat(rad)'].values
+    lambda_stations = ITRF['lon(rad)'].values
     
     # Initialiser le tableau des distances minimales
-    n_stations = len(ITDF)
+    n_stations = len(ITRF)
     distance_min = np.full(n_stations, np.inf)
     
     for i in range(n_stations):
@@ -39,8 +39,8 @@ def proxi(GRSM,ITDF):
         distance_min[i] = np.min(distances)
     
     # Ajouter les résultats au DataFrame
-    ITDF = ITDF.copy()
-    ITDF['in_deformation'] = distance_min < 50
-    ITDF['min_def'] = distance_min
-    return ITDF
+    ITRF = ITRF.copy()
+    ITRF['in_deformation'] = distance_min < 50
+    ITRF['min_def'] = distance_min
+    return ITRF
 
