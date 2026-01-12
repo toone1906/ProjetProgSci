c412187c38faa039b45e9f0d6139500490a52912
Theau LEFRANC <lefrancthea@gmail.com>
2026-01-12 18:15:06 +0100

    ajout des vecteurs

--- main.py
+++ main.py
@@ -20,17 +20,19 @@ d1 = time.time()
 
 print("--- Démarrage du chargement des données ---")
 with tqdm(total=3, desc="Chargement fichiers") as pbar:
+    
+    colspecs_ITRF = [(0, 9),(10, 25), (26,30),(32, 36),(37, 50),(51, 64),(65, 79),(80, 85),(86, 92),(93, 99),(100, 102),(103, 115),(116, 128)]
+    ITRF_2020 = pd.read_fwf("data/ITRF2020_GNSS.SSC.txt", skiprows=8,colspecs=colspecs_ITRF, names=["DOMES NB", "SITE NAME", "TECH","ID", "X/Vx","Y/Vy","Z/Vz","Sigma_x","Sigma_y","Sigma_z","SOLN","DATA_START","DATA_END"] )
+    pmm_itrf = pd.read_csv("data/pmm_itrf.txt",sep='\s+',skiprows=4,names=["Plate", "Name", "NS","Omega_x", "Omega_y","Omega_z","Omega","WRMS","Sigma_y","s_Omega_x","s_Omega_y","s_Omega_z","s_Omega"])
+    pbar.update(1)
+    
     with open("data/Tectonic_Plates.geojson") as f:
         Plaques_Techtoniques = json.load(f)
     pbar.update(1)
     
-    colspecs_ITRF = [(0, 9),(10, 25), (26,30),(32, 36),(37, 50),(51, 64),(65, 79),(80, 85),(86, 92),(93, 99),(100, 102),(103, 115),(116, 128)]
     GSRM = pd.read_fwf("data/GSRM_strain.txt", skiprows=25, names=["lat","long", "exx","eyy","exy","vorticity","RL-NLC","LL-NLC","e1","e2","azi_e1"] )
     pbar.update(1)
     
-    ITRF_2020 = pd.read_fwf("data/ITRF2020_GNSS.SSC.txt", skiprows=8,colspecs=colspecs_ITRF, names=["DOMES NB", "SITE NAME", "TECH","ID", "X/Vx","Y/Vy","Z/Vz","Sigma_x","Sigma_y","Sigma_z","SOLN","DATA_START","DATA_END"] )
-    pmm_itrf = pd.read_csv("data/pmm_itrf.txt",sep='\s+',skiprows=4,names=["Plate", "Name", "NS","Omega_x", "Omega_y","Omega_z","Omega","WRMS","Sigma_y","s_Omega_x","s_Omega_y","s_Omega_z","s_Omega"])
-    pbar.update(1)
 
 d2 = time.time()
 tqdm.write(f'Ouverture fichiers terminée en {d2-d1:.2f}s')
@@ -127,12 +129,12 @@ print(f'Temps calcul déformation : {d1-d:.2f}s')
 
 
 #4
-print(part4.v_pred(pmm_itrf,last_version_position2))
+last_version_position2 = part4.v_pred(pmm_itrf,last_version_position2)
 
 #5
 print("\nGénération des cartes...")
 
-part5.carte_monde_statique(dico_plaques_pmm_noms, last_version_position2, GSRM)
+#part5.carte_monde_statique(dico_plaques_pmm_noms, last_version_position2, GSRM)
 part5.carte_eurasie_statique(dico_plaques_pmm_noms, last_version_position2, GSRM)
 
 print("\n--- Terminée ---")
--- part5.py
+++ part5.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import numpy as np
 
 
 def carte_monde_statique(dico_plaques, df_stations, df_GSRM):
@@ -22,7 +23,8 @@ def carte_monde_statique(dico_plaques, df_stations, df_GSRM):
                           s=15,  # Points légèrement plus gros pour la vue zoomée
                           marker='o',
                           label='Deformation',
-                          zorder=5) 
+                          zorder=3,
+                          alpha=0.5) 
 
     scatter = ax.scatter(df_stations['lon(degres)'], df_stations['lat(degres)'],
                          transform=ccrs.PlateCarree(),
@@ -30,7 +32,7 @@ def carte_monde_statique(dico_plaques, df_stations, df_GSRM):
                          s=10, 
                          marker='o',
                          label='Stations GNSS',
-                         zorder=5) 
+                         zorder=10) 
     
     for i, (nom_plaque, df_plaque) in enumerate(dico_plaques.items()):
         color = colors[i % len(colors)]
@@ -38,7 +40,8 @@ def carte_monde_statique(dico_plaques, df_stations, df_GSRM):
                 transform=ccrs.PlateCarree(), 
                 label=nom_plaque, 
                 color=color, 
-                linewidth=1.5)
+                linewidth=1.5,
+                zorder=5)
 
     ax.set_title("Plaques Tectoniques et Stations GNSS")
 
@@ -74,7 +77,8 @@ def carte_eurasie_statique(dico_plaques, df_stations, df_GSRM):
                             s=15,  # Points légèrement plus gros pour la vue zoomée
                             marker='o',
                             label='Deformation',
-                            zorder=5) 
+                            zorder=3,
+                            alpha=0.5) 
 
     for i, (nom_plaque, df_plaque) in enumerate(dico_plaques.items()):
         color = colors[i % len(colors)]
@@ -88,7 +92,8 @@ def carte_eurasie_statique(dico_plaques, df_stations, df_GSRM):
                 label=nom_plaque, 
                 color=color, 
                 alpha=alpha,
-                linewidth=linewidth)
+                linewidth=linewidth,
+                zorder=5)
 
     scatter = ax.scatter(df_stations['lon(degres)'], df_stations['lat(degres)'],
                           transform=ccrs.PlateCarree(),
@@ -96,8 +101,28 @@ def carte_eurasie_statique(dico_plaques, df_stations, df_GSRM):
                           s=15,  # Points légèrement plus gros pour la vue zoomée
                           marker='o',
                           label='Stations GNSS',
-                          zorder=5) 
+                          zorder=10) 
+    SCALE_FACTOR = 0.05
     
+    q = ax.quiver(df_stations['lon(degres)'].values, df_stations['lat(degres)'].values,
+                  df_stations['Vx_pred'].values, df_stations['Vy_pred'].values,
+                  transform=ccrs.PlateCarree(),
+                  color='darkblue',        # Couleur des flèches
+                  width=0.002,             # Épaisseur du corps de la flèche
+                  headwidth=3,             # Largeur de la tête
+                  headlength=4,            # Longueur de la tête
+                  scale=SCALE_FACTOR,      # L'échelle (le plus important !)
+                  scale_units='inches',    # Unité de l'échelle pour rester cohérent au zoom
+                  zorder=15,               # Tout en haut
+                  label='Vitesse prédite')
+    REF_VELOCITY = 50 # La valeur de la flèche de référence (ex: 50 mm/an)
+    
+    ax.quiverkey(q, 
+                 X=0.9, Y=0.05,            # Position de la légende (en bas à droite)
+                 U=REF_VELOCITY,           # Longueur de la flèche de référence
+                 label=f'{REF_VELOCITY} mm/an', # Texte de la légende
+                 labelpos='E',             # Texte à l'Est de la flèche
+                 coordinates='axes', fontproperties={'size': 10})
     
 
     ax.set_title("Focus : Plaque Eurasie et Stations GNSS")
